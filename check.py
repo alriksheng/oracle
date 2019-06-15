c225ed67dbae3844da37f0298430d30e702a3520
dgg <s016374@aliyun.com>
2019-06-15 15:51:40 +0800

    py

---
+++ check.py
@@ -0,0 +1,12 @@
+写python
+Import cx_oracle
+Import pyunit
+
+Def xxx(date)
+     Result1 = Sql.execute(ssss)
+     Result2 = Sql.execute(ssss)
+     Println(result)
+     Pyunit.assert(result1 == result2) // true
+
+If __main__ == main
+xxx(date)
